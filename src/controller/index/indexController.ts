aeb0229237707c4a12324ffd0b5eada149d09ad1
Fadzar Wahyu <wahyufadz@gmail.com>
2021-01-23 11:02:53 +0800

    fix: test page

--- src/controller/index/indexController.ts
+++ src/controller/index/indexController.ts
@@ -8,4 +8,20 @@ export default async function indexController(fastify: FastifyInstance) {
   ) {
     reply.view("/page/index.ejs");
   });
+
+  // GET /test
+  fastify.get("/test", async function (
+    _request: FastifyRequest,
+    reply: FastifyReply
+  ) {
+    reply.send({
+      balance: "$3,277.32",
+      picture: "http://placehold.it/32x32",
+      age: 30,
+      name: "Leonor Cross",
+      gender: "female",
+      company: "GRONK",
+      email: "leonorcross@gronk.com",
+    });
+  });
 }
