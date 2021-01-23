ddcfa185654c2c121dc9721d6ab7a9eacb44e3c0
Fadzar Wahyu <wahyufadz@gmail.com>
2021-01-23 10:03:45 +0800

    feat: use prehandler to validate required form.body

--- src/controller/auth/authController.ts
+++ src/controller/auth/authController.ts
@@ -1,179 +1,217 @@
 import { compare, hash } from "bcrypt";
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import jwt, { decode } from 'jsonwebtoken';
-import { RevokeTokenService, UserService } from '../../service';
-import { identity as identitySchema, login, logout, refresh, register } from './authSchema';
+import {
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
+  HookHandlerDoneFunction,
+} from "fastify";
+import jwt, { decode } from "jsonwebtoken";
+import { RevokeTokenService, UserService } from "../../service";
+import {
+  identity as identitySchema,
+  login,
+  logout,
+  refresh,
+  register,
+} from "./authSchema";
 
 export default async function userController(fastify: FastifyInstance) {
-  const userService = new UserService()
-  const revokeTokenService = new RevokeTokenService()
-
-  const createToken = (user: any) => {
-    delete user.password
-    const token = jwt.sign({ user }, process.env.FASTIFY_JWT_TOKEN_SECRET || 'token secret string', { expiresIn: '2h' })
-    const refreshToken = jwt.sign({ user }, process.env.FASTIFY_JWT_REFRESH_SECRET || 'refresh token secret string', { expiresIn: '10d' })
-    return { token, refreshToken, user }
-  }
+  const userService = new UserService();
+  const revokeTokenService = new RevokeTokenService();
 
   // POST /api/v1/auth/login
-  fastify.post("/login", { schema: login }, async function (
-    request: FastifyRequest<{ Body: { identity: string, password: string } }>,
-    reply: FastifyReply,
-  ) {
-    const { identity, password } = request.body
+  fastify.post<{ Body: { identity: string; password: string } }>("/login", {
+    schema: login,
+    preHandler: checkRequiredParams(["identity", "password"]),
+    handler: async function (request, reply) {
+      const { identity, password } = request.body;
 
-    // check required fields
-    if (!(identity && password)) {
-      reply.badRequest('identity & password fields is required')
-      return
-    }
+      let user: any = await userService.readOne({
+        where: [{ email: identity }, { username: identity }],
+      });
 
-    let user: any = await userService.readOne({ where: [{ email: identity }, { username: identity }] })
+      // check is user registered
+      const isUserNotRegistered = user === undefined;
+      if (isUserNotRegistered) {
+        reply.unauthorized("user not registered");
+        return;
+      }
 
-    // check is user registered
-    const isUserNotRegistered = user === undefined
-    if (isUserNotRegistered) {
-      reply.unauthorized('user not registered')
-      return
-    }
-
-    // check password
-    const isPasswordNotMatch = !(await compare(password, user.password))
-    // if user found then compare password with bcrypt.compare
-    if (isPasswordNotMatch) {
-      reply.unauthorized('username/email & password doesn\'t match')
-      return
-    }
-    reply.send({
-      statusCode: 200,
-      message: 'login success',
-      value: createToken(user)
-    })
+      // check password
+      const isPasswordNotMatch = !(await compare(password, user.password));
+      // if user found then compare password with bcrypt.compare
+      if (isPasswordNotMatch) {
+        reply.unauthorized("username/email & password doesn't match");
+        return;
+      }
+      reply.send({
+        statusCode: 200,
+        message: "login success",
+        value: createToken(user),
+      });
+    },
   });
 
   // POST /api/v1/auth/register
-  fastify.post("/register", { schema: register }, async function (
-    request: FastifyRequest<{ Body: { username: string, email: string, firstName: string, lastName: string, password: string } }>,
-    reply: FastifyReply
-  ) {
-    const { username, email, firstName, lastName } = request.body
-    let { password } = request.body
+  fastify.post<{
+    Body: {
+      username: string;
+      email: string;
+      firstName: string;
+      lastName: string;
+      password: string;
+    };
+  }>("/register", {
+    schema: register,
+    preHandler: checkRequiredParams([
+      "username",
+      "email",
+      "firstName",
+      "lastName",
+      "password",
+    ]),
+    handler: async function (request, reply) {
+      const { username, email, firstName, lastName } = request.body;
+      let { password } = request.body;
 
-    // check required fields
-    if (!(username && email && firstName && lastName && password)) {
-      reply.badRequest('username, email, firstName, lastName, password fields is required')
-      return
-    }
-
-    // save data user to database
-    password = await hash(password, 10)
-    const user = await userService.create({ username, email, firstName, lastName, password });
-    reply.send({
-      statusCode: 201,
-      message: 'user created',
-      value: createToken(user)
-    })
+      // save data user to database
+      password = await hash(password, 10);
+      const user = await userService.create({
+        username,
+        email,
+        firstName,
+        lastName,
+        password,
+      });
+      reply.send({
+        statusCode: 201,
+        message: "user created",
+        value: createToken(user),
+      });
+    },
   });
 
   // POST /api/v1/auth/refresh
-  fastify.post("/refresh", { schema: refresh }, async function (
-    request: FastifyRequest<{ Body: { refreshToken: string } }>,
-    reply: FastifyReply
-  ) {
-    const { refreshToken } = request.body
+  fastify.post<{ Body: { refreshToken: string } }>("/refresh", {
+    schema: refresh,
+    preHandler: checkRequiredParams(["refreshToken"]),
+    handler: async function (request, reply) {
+      const { refreshToken } = request.body;
 
-    // check required fields
-    if (!(refreshToken)) {
-      reply.badRequest('refreshToken field is required')
-      return
-    }
+      const decoded: any = decode(refreshToken);
 
-    const decoded: any = decode(refreshToken)
+      // check if token was expired
+      const isTokenExpired =
+        Number(decoded.exp) < Math.floor(Date.now() / 1000);
+      if (isTokenExpired) {
+        reply.unauthorized("refresh token was expired please re-login");
+        return;
+      }
 
-    // check if token was expired
-    const isTokenExpired = Number(decoded.exp) < Math.floor(Date.now() / 1000)
-    if (isTokenExpired) {
-      reply.unauthorized('refresh token was expired please re-login')
-      return
-    }
+      // check refresh token is revoked / logged out
+      const isLoggedOut =
+        (await revokeTokenService.readOne({ token: refreshToken })) !==
+        undefined;
+      if (isLoggedOut) {
+        reply.unauthorized("refresh token was revoked please re-login");
+        return;
+      }
 
-    // check refresh token is revoked / logged out
-    const isLoggedOut = await revokeTokenService.readOne({ token: refreshToken }) !== undefined
-    if (isLoggedOut) {
-      reply.unauthorized('refresh token was revoked please re-login')
-      return
-    }
-
-    reply.send({
-      statusCode: 200,
-      message: 'token refresh success',
-      value: createToken(decoded.user)
-    })
-  })
+      reply.send({
+        statusCode: 200,
+        message: "token refresh success",
+        value: createToken(decoded.user),
+      });
+    },
+  });
 
   // POST /api/v1/auth/logout
-  fastify.post("/logout", { schema: logout }, async function (
-    request: FastifyRequest<{ Body: { refreshToken: string } }>,
-    reply: FastifyReply
-  ) {
-    const { refreshToken } = request.body
+  fastify.post<{ Body: { refreshToken: string } }>("/logout", {
+    schema: logout,
+    preHandler: checkRequiredParams(["refreshToken"]),
+    handler: async function (request, reply) {
+      const { refreshToken } = request.body;
 
-    // check required fields
-    if (!(refreshToken)) {
-      reply.badRequest('refreshToken field is required')
-      return
-    }
+      const decoded: any = decode(refreshToken);
 
-    const decoded: any = decode(refreshToken)
+      // check if token was expired
+      const isTokenExpired =
+        Number(decoded.exp) < Math.floor(Date.now() / 1000);
+      if (isTokenExpired) {
+        reply.unauthorized("refresh token was expired please re-login");
+        return;
+      }
 
-    // check if token was expired
-    const isTokenExpired = Number(decoded.exp) < Math.floor(Date.now() / 1000)
-    if (isTokenExpired) {
-      reply.unauthorized('refresh token was expired please re-login')
-      return
-    }
+      // check refresh token is revoked / logged out
+      const isLoggedOut =
+        (await revokeTokenService.readOne({ token: refreshToken })) !==
+        undefined;
+      if (isLoggedOut) {
+        reply.unauthorized("refresh token was revoked please re-login");
+        return;
+      }
 
-    // check refresh token is revoked / logged out
-    const isLoggedOut = await revokeTokenService.readOne({ token: refreshToken }) !== undefined
-    if (isLoggedOut) {
-      reply.unauthorized('refresh token was revoked please re-login')
-      return
-    }
-
-    // save refresh token as revoked
-    await revokeTokenService.create({ token: refreshToken })
-    reply.send({
-      statusCode: 200,
-      message: 'user was logged out',
-    })
-
-  })
+      // save refresh token as revoked
+      await revokeTokenService.create({ token: refreshToken });
+      reply.send({
+        statusCode: 200,
+        message: "user was logged out",
+      });
+    },
+  });
 
   // POST /api/v1/identity
-  fastify.post("/identity", { schema: identitySchema }, async function (
-    request: FastifyRequest<{ Body: { identity: string } }>,
-    reply: FastifyReply
-  ) {
-    const { identity } = request.body
+  fastify.post<{ Body: { identity: string } }>("/identity", {
+    schema: identitySchema,
+    preHandler: checkRequiredParams(["identity"]),
+    handler: async function (request, reply) {
+      const { identity } = request.body;
 
-    // check required fields
-    if (!(identity)) {
-      reply.badRequest('identity field is required')
-      return
-    }
+      // check if identity was taken
+      const isTaken =
+        (await userService.readOne({
+          where: [{ username: identity }, { email: identity }],
+        })) !== undefined;
+      if (isTaken) {
+        reply.forbidden("username or email was taken");
+        return;
+      }
 
-    // check if identity was taken
-    const isTaken = await userService.readOne({ where: [{ username: identity }, { email: identity }] }) !== undefined
-    if (isTaken) {
-      reply.forbidden('username or email was taken')
-      return
-    }
-
-    reply.send({
-      statusCode: 200,
-      message: 'username or email not registered yet'
-    })
-
-  })
-
+      reply.send({
+        statusCode: 200,
+        message: "username or email not registered yet",
+      });
+    },
+  });
 }
+
+const createToken = (user: any) => {
+  delete user.password;
+  const token = jwt.sign(
+    { user },
+    process.env.FASTIFY_JWT_TOKEN_SECRET || "token secret string",
+    { expiresIn: "2h" }
+  );
+  const refreshToken = jwt.sign(
+    { user },
+    process.env.FASTIFY_JWT_REFRESH_SECRET || "refresh token secret string",
+    { expiresIn: "10d" }
+  );
+  return { token, refreshToken, user };
+};
+
+const checkRequiredParams = <T>(params: string[]) => (
+  request: FastifyRequest<T>,
+  reply: FastifyReply,
+  next: HookHandlerDoneFunction
+) => {
+  const required: string[] = [];
+  params.forEach((param) => {
+    if (!(param in request.body)) required.push(param);
+  });
+  if (required.length) {
+    reply.badRequest(required.join(", ") + " is required");
+    return;
+  }
+  next();
+};
